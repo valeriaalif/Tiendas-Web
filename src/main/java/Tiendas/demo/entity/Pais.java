ee0b647e682d2949d94c8b83cd6ad441bb348472
Valeria Ali <vsalif123@gmail.com>
2022-11-18 14:40:28 -0600

    se crean los entities.

---
+++ src/main/java/Tiendas/demo/entity/Pais.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tiendas.demo.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "paises")
+public class Pais {
+    
+}
