a185e16bdd664856c2cdee1883b93e1bcdea1259
Valeria Ali <vsalif123@gmail.com>
2022-11-18 15:29:03 -0600

    cambios persona service.

--- src/main/java/Tiendas/demo/service/PersonaService.java
+++ src/main/java/Tiendas/demo/service/PersonaService.java
@@ -2,23 +2,35 @@
 package Tiendas.demo.service;
 
 import Tiendas.demo.entity.Pais;
+import Tiendas.demo.entity.Persona;
 import Tiendas.demo.repository.PaisRepository;
+import Tiendas.demo.repository.PersonaRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PersonaService implements IPaisService {
+public class PersonaService implements IPersonaService {
     
     @Autowired
-    private PaisRepository paisRepository;
-
-
-    @Override
-    public List<Pais> listCountry() {
-        public List<Pais> listCountry(){
-            return (List<Pais>)paisRepository.findAll();
-        }
+    private PersonaRepository personaRepository;
+    
+    public List<Persona> getAllPersona(){
+        
+        return null;
+        
+    }
+    
+    public Persona getPersonaById(long id){
+        
+        return null;
+        
+    }
+    
+    public void savePersona(Persona persona){
+        
+    }
+  
     }
 
 
