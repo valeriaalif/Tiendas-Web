10ccfef9bd1550e5c625412f4bebae378062ca5d
Valeria Ali <vsalif123@gmail.com>
2022-11-24 21:38:30 -0600

    se implementan todos los source packages. htmls y conexion a MySQL.

--- src/main/java/Tiendas/demo/controller/PersonaController.java
+++ src/main/java/Tiendas/demo/controller/PersonaController.java
@@ -5,10 +5,13 @@ import Tiendas.demo.entity.Pais;
 import Tiendas.demo.entity.Persona;
 import Tiendas.demo.service.IPaisService;
 import Tiendas.demo.service.IPersonaService;
+import Tiendas.demo.service.PersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 public class PersonaController {
@@ -33,6 +36,22 @@ public class PersonaController {
         model.addAttribute("persona", new Persona());
         model.addAttribute("paises", listaPaises);
         return "crear";
+    }
+    
+    @GetMapping("/save")
+    public String guardarPersona(@ModelAttribute Persona persona){
+        personaService.savePersona(persona);
+        return "redirect:/persona";
+        
+    }
+    
+    @GetMapping("/editPersona/(id)")
+    public String editarPersona(@PathVariable("id")Long id, Model model){
+       Persona persona = PersonaService.getPersonabyId(id);
+       List<Pais> listaPaises = paisService.listCountry();
+       model.addAttribute("persona", persona);
+       model.addAttribute("paises", listaPaises);
+       return "crear";
     }
     
 }
